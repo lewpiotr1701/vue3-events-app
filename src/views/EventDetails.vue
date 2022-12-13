7d051c50fa08ddd371deb6c9937c6bae68ffced4
Piotr <lewczykpiotr@gmail.com>
2022-12-13 20:26:52 +0100

    add EventDetails component

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>{{ event.title }}</h1>
+        <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+        <p>{{ event.description }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'EventDetails',
+    data() {
+        return {
+            event: null
+        }
+    },
+    created() {
+
+    }
+}
+</script>
