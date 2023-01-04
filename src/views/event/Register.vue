bd283d30cdeabcde07e894edb328294a560aa790
Piotr <lewczykpiotr@gmail.com>
2023-01-04 20:10:55 +0100

    add register button to event details register

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,10 +1,22 @@
 <template>
     <p>Register for the event</p>
+    <button @click="register">Register Me</button>
 </template>
 
 <script>
 export default {
     name: 'EventRegister',
     props: ['event'],
+    methods: {
+        register() {
+            // Call to API
+            // If registered then redirect to event details
+
+            this.$router.push({
+                name: 'EventDetails',
+                params: { id: this.event.id }
+            })
+        }
+    }
 }
 </script>
