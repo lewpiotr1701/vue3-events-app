0fc502f0d1ab2b117655fc7ead05c5a1ae3e0be0
Piotr <lewczykpiotr@gmail.com>
2023-01-04 18:54:05 +0100

    redirect routs /about and /event

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ const routes = [
     props: route => ({ page: parseInt(route.query.page) || 1 })
   },
   {
-    path: '/event/:id',
+    path: '/events/:id',
     name: 'EventLayout',
     props: true,
     component: EventLayout,
@@ -36,11 +36,17 @@ const routes = [
       }
     ]
   },
-
   {
-    path: '/about',
+    path: '/event/:afterEvent(.*)',
+    redirect: to => {
+      return { path: '/events/' + to.params.afterEvent }
+    }
+  },
+  {
+    path: '/about-us',
     name: 'About',
-    component: AboutView
+    component: AboutView,
+    alias: '/about'
   }
 ]
 
