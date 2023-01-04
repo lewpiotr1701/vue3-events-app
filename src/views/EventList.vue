6f9c2df7c8093e8796dce7c3e1a5e9cda639cb80
Piotr <lewczykpiotr@gmail.com>
2023-01-04 14:28:46 +0100

    add pages number to pagination

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -6,6 +6,15 @@
     <div class="pagination">
       <router-link id="page-prev" :to="{ name: 'EventList', query: { page: page - 1 } }" rel="prev"
         v-if="page != 1">&#60; Previous</router-link>
+
+      <ul class="pagination__numbers">
+        <li class="pagination__item" v-for="n in totalPages" :key="n">
+          <router-link :to="{ name: 'EventList', query: { page: n } }">
+            {{ n }}&nbsp;
+          </router-link>
+        </li>
+      </ul>
+
       <router-link id="page-next" :to="{ name: 'EventList', query: { page: page + 1 } }" rel="next"
         v-if="hasNextPage">Next &#62;</router-link>
     </div>
@@ -23,14 +32,16 @@ export default {
   components: {
     EventCard
   },
-  props: ['page'], // prop received from the router/index.js
+  props: ['page'], // prop received from the router/index.js using Props Function Mode
   data() {
     return {
       events: null,
-      totalEvents: 0
+      totalEvents: 0,
     }
   },
   created() {
+    // When reactive objects (page) that are accessed inside this function change,
+    // run this function again
     watchEffect(() => {
       this.events = null
       EventService.getEvents(2, this.page) // pass 2 events per page and current page
@@ -49,6 +60,9 @@ export default {
       const totalPages = Math.ceil(this.totalEvents / 2)
       // Check if the current page is less than the total pages
       return this.page < totalPages
+    },
+    totalPages() {
+      return Math.ceil(this.totalEvents / 2)
     }
   }
 }
@@ -64,7 +78,7 @@ export default {
 .pagination {
   width: 290px;
   display: flex;
-  justify-content: space-between;
+
 }
 
 .pagination a {
@@ -79,5 +93,16 @@ export default {
 
 #page-next {
   text-align: right;
+}
+
+.pagination__numbers {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+}
+
+.pagination__item {
+  display: inline;
 }
 </style>
